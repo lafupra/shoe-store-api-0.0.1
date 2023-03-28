c1017d08368f178ae60d3eaf259b29c788d0d0ed
praful <cpraful966@gmail.com>
2023-03-28 13:01:25 +0530

    it was just fault of two commas

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use("/api",PaymentRouter)
 
     app.listen(process.env.PORT,  () => {
 
-      console.log(process.env.MONGO_URI)
+     
       mongoose.set('strictQuery', false)
       mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser:true,
